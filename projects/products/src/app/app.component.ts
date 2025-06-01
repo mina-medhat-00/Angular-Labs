51e22fd087f40ba9b5d18c823b1efd950c700159
mina-medhat-00 <mina.medhat.3141@gmail.com>
2025-06-01 14:50:18 +0300

    update products homepage to include routing

--- projects/products/src/app/app.component.ts
+++ projects/products/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, RouterModule, NavbarComponent],
+  template: `<main class="main">
+    <app-navbar />
+    <div class="content">
+      <router-outlet></router-outlet>
+    </div>
+  </main> `,
   styles: ``,
 })
 export class AppComponent {}
--- projects/products/src/app/app.routes.ts
+++ projects/products/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { CartComponent } from './components/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home Page',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login Page',
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Register Page',
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+    title: 'Cart Page',
+  },
+];
